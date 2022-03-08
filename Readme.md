c12bbf9b384c32325386aaa5265d067c924463b2
alvalle09 <phredvalle@gmail.com>
2022-03-07 23:14:43 -0600

    create event handlers for play and adj volume

--- Readme.md
+++ Readme.md
@@ -1,7 +1,17 @@
 # AudioPlayer using React with Typescript
 
+## Agenda
 - create a music player app
 - integrate React with the Audio API
 - hook up the Audio API to a user's speaker system
 - play a track
 - control the playback and volume
+
+## How to use Audio API
+
+1. First, we create an AudioContext. This controls audio data and routes it where it's required.
+2. Create a source e.g a stream from an mp3 file.
+3. Create effect Nodes
+4. Provide a destination for the audio e.g. the computer's speakers.
+5. Connect sources to the effects, and effects to the destination.
+
---
+++ react-music/src/MusicPlayer.tsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+
+function MusicPlayer() {
+
+    const handlePlay = () : void => {
+        console.log('handle play')
+        // Todo: hanlde play
+    }
+
+    const changeVolume = (e: any) : void => {
+        console.log(`updating volume to ${e.target.value}`)
+        // Todo: adjust volume
+    }
+
+    return (
+        <div className="container">
+          <div className="row">
+            <div 
+            className="col-xs-3 col-md-4" />
+            <div 
+            className="col-xs-6 col-md-4">
+              ...
+              <div 
+              className="
+               col-xs-12 text-center">
+                <div className="
+                 btn-group btn-group-lg">
+                  <button
+                    type="button"
+                    className="
+                     btn btn-primary mr-2"
+                    onClick={handlePlay}
+                  >
+                    Play/Pause
+                  </button>
+                  <input
+                    type="range"
+                    id="volume"
+                    name="volume"
+                    className="
+                     control-volume"
+                    min="0"
+                    max="2"
+                    defaultValue="1"
+                    list="gain-vals"
+                    step="0.01"
+                    onChange={changeVolume}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="
+             col-xs-3 col-md-4" />
+          </div>
+        </div>
+      )
+
+}
+
+export default MusicPlayer
+
